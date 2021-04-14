a67d71ee8dfe5fec993077674a7fc21dc811ff46
sungbum1004 <tlsqkfka96@naver.com>
2021-04-15 03:00:09 +0900

    명령어 입력 기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("안녕하세요.")
+    println("== 게시판 관리 프로그램 시작==")
+
+    while ( true ) {
+        print("명령어) ")
+        val command = readLine()
+        println("입력한 명령어 : $command")
+    }
+
+
+    println("== 게시판 관리 프로그램 끝==")
+
 }
