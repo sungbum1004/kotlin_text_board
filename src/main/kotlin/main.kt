9d20c66166c3cb4ae5b7dab8cb1c7fcc983dd9ab
sungbum1004 <tlsqkfka96@naver.com>
2021-04-22 01:19:19 +0900

    게시물 입력기능 구현

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,38 @@
+fun readLineTrime() = readLine()!!.trim()
+
+
 fun main() {
     println("== 게시판 관리 프로그램 시작 ==")
+
+    var articlesLastId = 0
+
     while ( true ) {
         print("명령어) ")
-        val command = readLine()
-        println("입력한 명령어 : $command")
+        val command = readLineTrime()
 
         if ( command == "system exit" ) {
             println("프로그램을 종료합니다.")
             break
         }
+
+        else if ( command == "article write" ) {
+            val id = articlesLastId + 1
+            print("제목 : ")
+            val title = readLineTrime()
+            print("내용 : ")
+            val body = readLineTrime()
+            val article = Article(id, title, body)
+
+            println("${id}번 게시물이 작성되었습니다.")
+            articlesLastId = id
+
+        }
     }
 
     println("== 게시판 관리 프로그램 끝 ==")
 }
+data class Article(
+    val id: Int,
+    val title: String,
+    val body: String
+)
